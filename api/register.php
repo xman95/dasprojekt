21178cf9723524bb29723d294563d4b9ee4f50fb
Martinsss <martinszecsko@gmail.com>
2017-10-25 13:29:09 +0200

    Regisztrációs form
    Alap regisztrációs form 1.0.0-alpha

---
+++ api/register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8"/>
+<title>CappleX | Sheets API</title>
+</head>
+<body>
+<!-- Form -->
+<div>
+<form  id="sheets" name="sheets">
+<input id="first_name" name="last_name" value="" placeholder="First Name"/>
+<input id="last_name" name="last_name" value="" placeholder="Last Name"/>
+<input id="email" name="email" value="" placeholder="Email Address"/>
+<input id="submit" name="submit" type="button" value="Submit" onClick="submit_form()">
+</form>
+</div>
+<!-- Form -->
+</body>
+</html>
